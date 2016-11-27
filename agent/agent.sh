b5d5f022c6e86aa5e5178376da064368a0464a5a
Martynas G <martynas.gryb@gmail.com>
2016-11-27 17:00:41 +0200

    agent:  added curl loop in shell agent

--- agent/agent.sh
+++ agent/agent.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo 'hey'
+
+HOST='0.0.0.0'
+PORT='8080'
+
+COMMAND="curl --silent http://$HOST:$PORT" # add -v for verbose; "--stderr -" to redirect err to out
+echo $COMMAND
+
+while true
+do
+RESP=`$COMMAND`
+echo $RESP
+# todo RESP parsing
+SLEEP=
+sleep "${SLEEP:-5}"
+done
