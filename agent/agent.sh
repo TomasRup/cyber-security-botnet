c19063df003b48373eca83f1f1b1a943257034cd
Tomas Petras Rupsys <tprupsys@gmail.com>
2016-12-06 13:16:42 +0200

    Unix agent fix

--- agent/agent.sh
+++ agent/agent.sh
@@ -4,7 +4,7 @@ set -e
 HOST=10.3.3.250:8080/commands
 TIMEOUT=3
 
-while [[ true ]]; do
+while true; do
     EXECUTE_COMMANDS=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" $HOST)
     eval $EXECUTE_COMMANDS
     sleep $TIMEOUT
